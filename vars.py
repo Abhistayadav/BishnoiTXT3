822f17aa63cc087c5fca13d3fb8ddcf71f782794
MR. YADAV <163430575+yadavanu63@users.noreply.github.com>
2024-04-22 15:52:27 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,3 +1,3 @@
 api_id = "27464924"
 api_hash = "f28d089001481597f045b70a0daa73e4"
-bot_token = "7111682526:AAFhqayM5UJbgOGqcQ-x2KYzxzFaSsI0U3s"
+bot_token = "7002793618:AAEs6QwgZokG5TYEhgSK0RyqjREpbiyInS8"
