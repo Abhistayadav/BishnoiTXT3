4ce89ee51a8a92343af4548e9f4c0610f81575fb
Abhistayadav <167625506+Abhistayadav@users.noreply.github.com>
2024-04-25 22:09:14 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,3 +1,3 @@
 api_id = "27464924"
 api_hash = "f28d089001481597f045b70a0daa73e4"
-bot_token = "7137577409:AAFuaXKaVx8k80xAaEHiEdxdiGcXMxtl6Fg"
+bot_token = "6916989163:AAG9iSfwtl6vR8jgEOgLkvBF_Ofi4Bi1MfI"
